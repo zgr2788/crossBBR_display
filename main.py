be3f8b85df651995f2c5279962d5ae4b1074ee80
Özgür Yılmaz Beker <89029806+zgr2788@users.noreply.github.com>
2022-12-24 14:24:32 +0000

    Modify main for future services

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import sys
 
 try:
-    gene_id = sys.argv[1]
-    scale_type = sys.argv[2]
+    service = sys.argv[1]
+    gene_id = sys.argv[2]
+    scale_type = sys.argv[3]
 except:
     print("Insufficient args")
     sys.exit()
 
 import services as _services
 
+if service == "ctr":
+    try:
+        counts_dict, gene_id = _services.fetchCounts(gene_id)
+    except:
+        print("Gene ID not recognized")
+        sys.exit()
+    _services.fetchCountsPlot(counts_dict, gene_id, scale_type)
 
-try:
-    counts_dict, gene_id = _services.fetchCounts(gene_id)
-except:
-    print("Gene ID not recognized")
+else:
+    print("Service not recognized")
     sys.exit()
-
-_services.fetchCountsPlot(counts_dict, gene_id, scale_type)
