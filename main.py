f995a56dc8faa1fb2a3caff3b7685441ee6a8204
Özgür Yılmaz Beker <89029806+zgr2788@users.noreply.github.com>
2022-12-25 00:49:58 +0000

    Change scale implementation for parallel

--- main.py
+++ main.py
@@ -4,19 +4,16 @@ import fastapi.templating as _templates
 import fastapi.staticfiles as _StaticFiles
 from starlette.responses import RedirectResponse
 import jinja2 as _jinja2
-import os
 
 app = _fastapi.FastAPI()
 app.mount("/static", _StaticFiles.StaticFiles(directory="Static"), name="Static")
 templates = _templates.Jinja2Templates(directory = "Templates")
 
-scale_mode = "log1p"
-
 # Main page
 @app.get("/")
 async def main_page(request: _fastapi.Request):
     gene_list = await _services.fetchGeneList()
-    return templates.TemplateResponse('display_home.html', context = {'request' : request, 'genes_df' : gene_list, 'scale_mode' : scale_mode})
+    return templates.TemplateResponse('display_home.html', context = {'request' : request, 'genes_df' : gene_list})
 
 # Main page search function
 @app.post("/")
@@ -27,33 +24,21 @@ async def search_main(request: _fastapi.Request, search_string : str = _fastapi.
     if search_string:
         gene_list = gene_list[gene_list["gene_names"].str.contains(search_string, case=False)]
 
-    return templates.TemplateResponse('display_home.html', context = {'request' : request, 'genes_df' : gene_list, 'scale_mode' : scale_mode})
-
-# Set scaling
-@app.get("/setscale/{mode}")
-async def set_scaling(request: _fastapi.Request, mode : str):
-    global scale_mode
-    scale_mode = mode
-
-    response = RedirectResponse(url='/')
-
-    return response
+    return templates.TemplateResponse('display_home.html', context = {'request' : request, 'genes_df' : gene_list})
 
 
-@app.get("/barplot/{gene_id}")
-async def bar_plot(request: _fastapi.Request, gene_id: str, btnradio = _fastapi.Form(False)):
+@app.get("/barplot/{gene_id}/{norm}")
+async def bar_plot(request: _fastapi.Request, gene_id: str, norm : str):
     counts_dict, gene_id = await _services.fetchCounts(gene_id)
     
-    # TODO: Implement scaling change
-    await _services.fetchCountsPlot(counts_dict, gene_id, scale_mode)
+    await _services.fetchCountsPlot(counts_dict, gene_id, norm)
 
-    return templates.TemplateResponse(gene_id + "_counts_" + scale_mode + ".html", context={'request' : request})
+    return templates.TemplateResponse(gene_id + "_counts_" + norm + ".html", context={'request' : request})
 
-@app.get("/boxplot/{gene_id}")
-async def box_plot(request: _fastapi.Request, gene_id: str):
+@app.get("/boxplot/{gene_id}/{norm}")
+async def box_plot(request: _fastapi.Request, gene_id: str, norm : str):
     counts_dict, gene_id = await _services.fetchCounts(gene_id)
     
-    # TODO: Implement scaling change
-    await _services.fetchCountsBoxPlot(counts_dict, gene_id, scale_mode)
+    await _services.fetchCountsBoxPlot(counts_dict, gene_id, norm)
 
-    return templates.TemplateResponse(gene_id + "_counts_boxplot_" + scale_mode + ".html", context={'request' : request})
+    return templates.TemplateResponse(gene_id + "_counts_boxplot_" + norm + ".html", context={'request' : request})
