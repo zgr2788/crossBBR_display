ea9fa974ff89e113a51e6f79619bfb2135d39050
Özgür Yılmaz Beker <89029806+zgr2788@users.noreply.github.com>
2023-02-13 18:35:19 +0000

    Prefetch for counts to reduce server-side load

--- services.py
+++ services.py
@@ -378,104 +378,104 @@ async def fetchCountsIntraVariancePlot(gene_id, zero_filt = False, count_dict = 
 # Precondition : counts -> count_dict primitive, NOT from fetchCounts
 # Returns : Sample count distribution in runs per tissue  
 async def fetchSampleCountDistrib(gene_id, zero_filt = False, count_dict = count_dict, scale_type = "log1p", include_cs = False):
-    
-    graph_df_list = []
-    
-    # Switch brain to 1st level
-    if tissues[0] != "Brain":
-        idx = 1
+    if not os.path.exists("Templates/" + gene_id + "_counts_whisk_" + scale_type + "_csexc.html"):
+        graph_df_list = []
         
-        while tissues[idx] != "Brain":
-            idx += 1
+        # Switch brain to 1st level
+        if tissues[0] != "Brain":
+            idx = 1
+            
+            while tissues[idx] != "Brain":
+                idx += 1
+            
+            temp = tissues[0]
+            tissues[0] = tissues[idx]
+            tissues[idx] = temp 
+    
+        # Get whisker plot data for each tissue
+        for tissue in tissues:
+            graph_df = _pd.DataFrame(columns = ["Tissue", "Count", "lower", "middle", "upper"])
+            tiss_samples = [key for key in list(sample_tissue_map.keys()) if sample_tissue_map[key] == tissue]
+    
+            # Construct data corpus
+            for id, df in count_dict.items():
+            
+                if id not in ["Counts_cs_select.csv", "Counts_nocs_select.csv", "Counts_raw.csv"]:
+                
+                    if include_cs or ("nocs" in id):
+                    
+                        try:
+                            gene_series = df.loc[gene_id]
+                            sample_mask = [1 if gene_series.index[i] in tiss_samples else 0 for i in range(len(gene_series.index))]
+                            gene_series_filt = [gene_series[i] for i in range(len(gene_series)) if sample_mask[i] == 1]
+    
+                            if zero_filt:
+                                gene_series_filt = [count for count in gene_series_filt if count != 0]
+    
+                            gene_series_norm = _np.log1p(gene_series_filt)
+    
+                            lower, middle, upper = (_np.quantile(gene_series_norm, quant) for quant in [0.25, 0.5, 0.75])
+    
+                            cur_df = _pd.DataFrame.from_dict({
+                                "Tissue" : [tissue]*len(gene_series_norm), 
+                                "Count" : gene_series_norm, 
+                                "lower" : lower,
+                                "middle" : middle, 
+                                "upper" : upper 
+                            })
+    
+                            graph_df = _pd.concat([graph_df, cur_df])
+    
+                        except:
+                            return "Gene ID not found in one of the count matrices!"
+            
+            graph_df_list.append(graph_df)
         
-        temp = tissues[0]
-        tissues[0] = tissues[idx]
-        tissues[idx] = temp 
-
-    # Get whisker plot data for each tissue
-    for tissue in tissues:
-        graph_df = _pd.DataFrame(columns = ["Tissue", "Count", "lower", "middle", "upper"])
-        tiss_samples = [key for key in list(sample_tissue_map.keys()) if sample_tissue_map[key] == tissue]
-
-        # Construct data corpus
-        for id, df in count_dict.items():
-
-            if id not in ["Counts_cs_select.csv", "Counts_nocs_select.csv", "Counts_raw.csv"]:
-
-                if include_cs or ("nocs" in id):
-
-                    try:
-                        gene_series = df.loc[gene_id]
-                        sample_mask = [1 if gene_series.index[i] in tiss_samples else 0 for i in range(len(gene_series.index))]
-                        gene_series_filt = [gene_series[i] for i in range(len(gene_series)) if sample_mask[i] == 1]
-
-                        if zero_filt:
-                            gene_series_filt = [count for count in gene_series_filt if count != 0]
-
-                        gene_series_norm = _np.log1p(gene_series_filt)
-
-                        lower, middle, upper = (_np.quantile(gene_series_norm, quant) for quant in [0.25, 0.5, 0.75])
-
-                        cur_df = _pd.DataFrame.from_dict({
-                            "Tissue" : [tissue]*len(gene_series_norm), 
-                            "Count" : gene_series_norm, 
-                            "lower" : lower,
-                            "middle" : middle, 
-                            "upper" : upper 
-                        })
-
-                        graph_df = _pd.concat([graph_df, cur_df])
-
-                    except:
-                        return "Gene ID not found in one of the count matrices!"
+        # Get plot y axis limit
+        y_max = 0
+    
+        for df in graph_df_list:
+            y_max = max(y_max, max([num for num in df.to_numpy().flatten() if isinstance(num, float)]))
+    
         
-        graph_df_list.append(graph_df)
+        # Construct the plot
+        if include_cs: # Unused
+            _io.output_file("Templates/" + gene_id + "_counts_whisk_" + scale_type + ".html", title=gene_id + "_counts_whisk_" + scale_type)
+        else:
+            _io.output_file("Templates/" + gene_id + "_counts_whisk_" + scale_type + "_csexc.html", title=gene_id + "_counts_whisk_" + scale_type)
+        
+        _plot.curdoc().theme = 'light_minimal'
     
-    # Get plot y axis limit
-    y_max = 0
-
-    for df in graph_df_list:
-        y_max = max(y_max, max([num for num in df.to_numpy().flatten() if isinstance(num, float)]))
-
+        p = _plot.figure(
+            x_range=_plotmod.FactorRange(*tissues, bounds="auto"),
+            title= gene_id + " counts sample variance across Tissues",
+            y_axis_label="Normalized counts (log1p)",
+            x_axis_label="Tissues",
+            width = int(1920 * 0.9), 
+            height= int(1080 * 0.9),
+            sizing_mode='scale_width',
+            y_range=_plotmod.Range1d(0, y_max+1, bounds="auto")
+        )
     
-    # Construct the plot
-    if include_cs: # Unused
-        _io.output_file("Templates/" + gene_id + "_counts_whisk_" + scale_type + ".html", title=gene_id + "_counts_whisk_" + scale_type)
-    else:
-        _io.output_file("Templates/" + gene_id + "_counts_whisk_" + scale_type + "_csexc.html", title=gene_id + "_counts_whisk_" + scale_type)
+        for df in graph_df_list:
+            source = _plotmod.ColumnDataSource(df)
     
-    _plot.curdoc().theme = 'light_minimal'
-
-    p = _plot.figure(
-        x_range=_plotmod.FactorRange(*tissues, bounds="auto"),
-        title= gene_id + " counts sample variance across Tissues",
-        y_axis_label="Normalized counts (log1p)",
-        x_axis_label="Tissues",
-        width = int(1920 * 0.9), 
-        height= int(1080 * 0.9),
-        sizing_mode='scale_width',
-        y_range=_plotmod.Range1d(0, y_max+1, bounds="auto")
-    )
-
-    for df in graph_df_list:
-        source = _plotmod.ColumnDataSource(df)
-
-        whisker = _plotmod.Whisker(base="Tissue", upper="upper", lower="lower", source=source, level="annotation", line_width = 1)
-        mid = _plotmod.Whisker(base="Tissue", upper="middle", lower="middle", source=source, level="annotation", line_width = 2)
-        whisker.upper_head.size = whisker.lower_head.size = 20
-        mid.upper_head.size = mid.lower_head.size = 40
-        mid.upper_head.line_color = mid.lower_head.line_color = 'red'
-        p.add_layout(whisker)
-        p.add_layout(mid)
-
-        cmap = _trans.factor_cmap("Tissue", palette=Category20[len(tissues)], factors=tissues)
-        p.circle(_trans.jitter("Tissue", 0.3, range=p.x_range), "Count", source=df,
-            alpha=0.2, size=13, line_color="white",
-            color=cmap)
-
-    p.y_range.start = 0
-    p.x_range.range_padding = 0.1
-    p.xaxis.major_label_orientation = 1
-    p.xgrid.grid_line_color = None
-
-    _io.save(p)
+            whisker = _plotmod.Whisker(base="Tissue", upper="upper", lower="lower", source=source, level="annotation", line_width = 1)
+            mid = _plotmod.Whisker(base="Tissue", upper="middle", lower="middle", source=source, level="annotation", line_width = 2)
+            whisker.upper_head.size = whisker.lower_head.size = 20
+            mid.upper_head.size = mid.lower_head.size = 40
+            mid.upper_head.line_color = mid.lower_head.line_color = 'red'
+            p.add_layout(whisker)
+            p.add_layout(mid)
+    
+            cmap = _trans.factor_cmap("Tissue", palette=Category20[len(tissues)], factors=tissues)
+            p.circle(_trans.jitter("Tissue", 0.3, range=p.x_range), "Count", source=df,
+                alpha=0.2, size=13, line_color="white",
+                color=cmap)
+    
+        p.y_range.start = 0
+        p.x_range.range_padding = 0.1
+        p.xaxis.major_label_orientation = 1
+        p.xgrid.grid_line_color = None
+    
+        _io.save(p)
