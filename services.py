7e2df63a1de1c52e16e9fcb8c9e32ade813b741e
Özgür Yılmaz Beker <89029806+zgr2788@users.noreply.github.com>
2023-02-13 13:04:19 +0000

    Add whisker plot base

--- services.py
+++ services.py
@@ -11,6 +11,7 @@ import bokeh.transform as _trans
 from bokeh.palettes import Category20, Category10
 from bokeh.sampledata.autompg2 import autompg2
 
+# Folder metadata
 counts = "Counts/"
 aggreg = "Aggregs/"
 
@@ -30,7 +31,7 @@ with open("Counts/var_info_nonzero.pickle", "rb") as f:
 count_dict = {fname : _pd.read_csv(counts + fname, index_col=0) for fname in os.listdir(counts) if ".csv" in fname}
 sample_table = _pd.read_csv("sampleTable_final_ideal_dots.csv")
 sample_tissue_map = { sample_table["SRR_ID"][i] : sample_table["Tissue_type"][i] for i in range(len(sample_table))}
-tissues = set(list(sample_tissue_map.values()))
+tissues = list(set(list(sample_tissue_map.values())))
 
 # Read and construct aggregation reference
 aggreg_ref = _pd.read_csv("Aggregs/aggreg.csv")
@@ -193,6 +194,7 @@ async def fetchCountsBoxPlot(counts_dict, gene_id, scale_type = "log1p", include
     fnames = list(counts_dict.keys())
     cols = list(counts_dict["Counts_raw.csv"].keys())
 
+    # Switch brain to 1st level
     if cols[0] != "Brain":
         idx = 1
         
@@ -365,11 +367,6 @@ async def fetchCountsIntraVariancePlot(gene_id, zero_filt = False, count_dict = 
     p.circle(_trans.jitter("Run", 0.3, range=p.x_range), "Count", source=graph_df,
          alpha=0.5, size=13, line_color="white",
          color=cmap)
-    #p.vbar("Run", 0.7, "q2", "q3", source=source, color=cmap, line_color="black")
-    #p.vbar("Run", 0.7, "q1", "q2", source=source, color=cmap, line_color="black")
-
-    #outliers = graph_df[~graph_df.Count.between(graph_df.lower, graph_df.upper)]
-    #p.scatter("Run", "Count", source=outliers, size=6, color="black", alpha=0.3)
 
     p.y_range.start = 0
     p.x_range.range_padding = 0.1
@@ -377,3 +374,111 @@ async def fetchCountsIntraVariancePlot(gene_id, zero_filt = False, count_dict = 
     p.xgrid.grid_line_color = None
 
     _io.save(p)
+
+# Precondition : counts -> count_dict primitive, NOT from fetchCounts
+# Returns : Sample count distribution in runs per tissue  
+def fetchSampleCountDistrib(gene_id, zero_filt = False, count_dict = count_dict, scale_type = "log1p", include_cs = False):
+    
+    graph_df_list = []
+    
+    # Switch brain to 1st level
+    if tissues[0] != "Brain":
+        idx = 1
+        
+        while tissues[idx] != "Brain":
+            idx += 1
+        
+        temp = tissues[0]
+        tissues[0] = tissues[idx]
+        tissues[idx] = temp 
+
+    # Get whisker plot data for each tissue
+    for tissue in tissues:
+        graph_df = _pd.DataFrame(columns = ["Tissue", "Count", "lower", "middle", "upper"])
+        tiss_samples = [key for key in list(sample_tissue_map.keys()) if sample_tissue_map[key] == tissue]
+
+        # Construct data corpus
+        for id, df in count_dict.items():
+
+            if id not in ["Counts_cs_select.csv", "Counts_nocs_select.csv", "Counts_raw.csv"]:
+
+                if include_cs or ("nocs" in id):
+
+                    try:
+                        gene_series = df.loc[gene_id]
+                        sample_mask = [1 if gene_series.index[i] in tiss_samples else 0 for i in range(len(gene_series.index))]
+                        gene_series_filt = [gene_series[i] for i in range(len(gene_series)) if sample_mask[i] == 1]
+
+                        if zero_filt:
+                            gene_series_filt = [count for count in gene_series_filt if count != 0]
+
+                        gene_series_norm = _np.log1p(gene_series_filt)
+
+                        lower, middle, upper = (_np.quantile(gene_series_norm, quant) for quant in [0.25, 0.5, 0.75])
+
+                        cur_df = _pd.DataFrame.from_dict({
+                            "Tissue" : [tissue]*len(gene_series_norm), 
+                            "Count" : gene_series_norm, 
+                            "lower" : lower,
+                            "middle" : middle, 
+                            "upper" : upper 
+                        })
+
+                        graph_df = _pd.concat([graph_df, cur_df])
+
+                    except:
+                        return "Gene ID not found in one of the count matrices!"
+        
+        graph_df_list.append(graph_df)
+    
+    # Get plot y axis limit
+    y_max = 0
+
+    for df in graph_df_list:
+        y_max = max(y_max, max([num for num in df.to_numpy().flatten() if isinstance(num, float)]))
+
+    
+    # Construct the plot
+    if include_cs: # Unused
+        _io.output_file("Templates/" + gene_id + "_counts_whisk_" + scale_type + ".html", title=gene_id + "_counts_whisk_" + scale_type)
+    else:
+        _io.output_file("Templates/" + gene_id + "_counts_whisk_" + scale_type + "_csexc.html", title=gene_id + "_counts_whisk_" + scale_type)
+    
+    _plot.curdoc().theme = 'light_minimal'
+
+    p = _plot.figure(
+        x_range=_plotmod.FactorRange(*tissues, bounds="auto"),
+        title= gene_id + " counts sample variance across Tissues",
+        y_axis_label="Normalized counts (log1p)",
+        x_axis_label="Tissues",
+        width = int(1920 * 0.9), 
+        height= int(1080 * 0.9),
+        sizing_mode='scale_width',
+        y_range=_plotmod.Range1d(0, y_max, bounds="auto")
+    )
+
+    for df in graph_df_list:
+        source = _plotmod.ColumnDataSource(df)
+
+        whisker = _plotmod.Whisker(base="Tissue", upper="upper", lower="lower", source=source, level="annotation", line_width = 2)
+        whisker.upper_head.size = whisker.lower_head.size = 20
+        p.add_layout(whisker)
+
+        cmap = _trans.factor_cmap("Tissue", palette=Category20[len(tissues)], factors=tissues)
+        p.circle(_trans.jitter("Tissue", 0.3, range=p.x_range), "Count", source=df,
+            alpha=0.5, size=13, line_color="white",
+            color=cmap)
+
+    p.y_range.start = 0
+    p.x_range.range_padding = 0.1
+    p.xaxis.major_label_orientation = 1
+    p.xgrid.grid_line_color = None
+
+    _io.save(p)
+    
+
+
+
+geneid="ENSG00000167889"
+
+print(fetchSampleCountDistrib(geneid))
