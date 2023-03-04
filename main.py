a3c8013d6fe97005ad986898f745825995ab7ca1
Özgür Yılmaz Beker <89029806+zgr2788@users.noreply.github.com>
2023-03-04 16:04:00 +0000

    Add responsive table, redo table displays

--- main.py
+++ main.py
@@ -21,136 +21,22 @@ async def landing(request: _fastapi.Request):
 
 # CS excluded results page
 @app.get("/omics")
-async def ex_main_page(request: _fastapi.Request, rnaseq_disp : Union[str, None] = _fastapi.Cookie(default="All")):
-    gene_list = await _services.fetchExGeneList(rnaseq_disp)
-    return templates.TemplateResponse('display_ex_home.html', context = {'request' : request, 'genes_df' : gene_list, 'rnaseq_disp' : rnaseq_disp})
-
-# Search CS excluded results
-@app.post("/omics")
-async def ex_main_search(request: _fastapi.Request, search_string : str = _fastapi.Form(), rnaseq_disp : Union[str, None] = _fastapi.Cookie(default="All")):
-    gene_list = await _services.fetchExGeneList(rnaseq_disp)
-
-    if search_string:
-        gene_list = gene_list[gene_list["gene_names"].str.contains(search_string, case=False)]
-
-    return templates.TemplateResponse('display_ex_home.html', context = {'request' : request, 'genes_df' : gene_list, 'rnaseq_disp' : rnaseq_disp})
-
-# CS excluded (reset route)
-@app.get("/omics/reset")
-async def ex_main_reset(request: _fastapi.Request, rnaseq_disp : Union[str, None] = _fastapi.Cookie(default="All")):
-    response = RedirectResponse("/omics")
-    response.set_cookie(key="rnaseq_disp", value="All")
-    return response
-
-# CS excluded (deseq2 only route)
-@app.get("/omics/deseq2")
-async def ex_main_deseq2(request: _fastapi.Request, rnaseq_disp : Union[str, None] = _fastapi.Cookie(default="All")):
-    response = RedirectResponse("/omics")
-    response.set_cookie(key="rnaseq_disp", value="DESeq2")
-    return response
-
-# CS excluded (deseq2 valid route)
-@app.get("/omics/valid/deseq2")
-async def ex_main_deseq2_valid(request: _fastapi.Request, rnaseq_disp : Union[str, None] = _fastapi.Cookie(default="All")):
-    response = RedirectResponse("/omics")
-    response.set_cookie(key="rnaseq_disp", value="DESeq2 Validation")
-    return response
-
-# CS excluded (wilcox valid route)
-@app.get("/omics/valid/wilcox")
-async def ex_main_deseq2_valid(request: _fastapi.Request, rnaseq_disp : Union[str, None] = _fastapi.Cookie(default="All")):
-    response = RedirectResponse("/omics")
-    response.set_cookie(key="rnaseq_disp", value="Wilcox Validation")
-    return response
-
-# CS excluded (wilcox only route)
-@app.get("/omics/wilcox")
-async def ex_main_wilcox(request: _fastapi.Request, rnaseq_disp : Union[str, None] = _fastapi.Cookie(default="All")):
-    response = RedirectResponse("/omics")
-    response.set_cookie(key="rnaseq_disp", value="Wilcox")
-    return response
-
-# For count barplots
-@app.get("/omics/plots/barplot/{gene_id}")
-async def bar_plot(request: _fastapi.Request, gene_id: str):
-
-    counts_dict, gene_id = await _services.fetchCounts(gene_id, include_cs=False)
-    
-    await _services.fetchCountsPlot(counts_dict, gene_id, include_cs=False)
-
-    return templates.TemplateResponse(gene_id + "_counts_log1p" + "_csexc.html", context={'request' : request})
+async def ex_main_page(request: _fastapi.Request):
+    gene_list = await _services.fetchExGeneList()
+    return templates.TemplateResponse('display_ex_home.html', context = {'request' : request, 'genes_df' : gene_list[0], 'genes_deseq2_df' : gene_list[1], 'genes_deseq2_valid_df' : gene_list[2],'genes_wilcox_df' : gene_list[3], 'genes_wilcox_valid_df' : gene_list[4]})
 
 # For intra-sample variance
 @app.get("/omics/plots/intravar/{gene_id}")
-async def intravar_box_plot(request: _fastapi.Request, gene_id: str):
-    counts_dict, gene_id = await _services.fetchCounts(gene_id, include_cs=False)
-    
+async def intravar_box_plot(request: _fastapi.Request, gene_id: str):    
     await _services.fetchCountsIntraVariancePlot(gene_id, zero_filt=True, include_cs=False)
 
     return templates.TemplateResponse(gene_id + "_counts_intravar_box_log1p" + "_csexc.html", context={'request' : request})
 
 # For count plots with sample info
 @app.get("/omics/plots/counts/{gene_id}/")
-async def counts_plot(request: _fastapi.Request, gene_id: str):
-
-    counts_dict, gene_id = await _services.fetchCounts(gene_id, include_cs=False)
-    
+async def counts_plot(request: _fastapi.Request, gene_id: str):    
     await _services.fetchSampleCountDistrib(gene_id, include_cs=False)
 
     return templates.TemplateResponse(gene_id + "_counts_whisk_log1p" + "_csexc.html", context={'request' : request})
 
 #################################################################
-"""
-# CS included results page
-@app.get("/csinc")
-async def main_page(request: _fastapi.Request):
-    gene_list = await _services.fetchGeneList()
-    return templates.TemplateResponse('display_home.html', context = {'request' : request, 'genes_df' : gene_list})
-
-# CS included results page search function
-@app.post("/csinc")
-async def search_main(request: _fastapi.Request, search_string : str = _fastapi.Form()):
-
-    gene_list = await _services.fetchGeneList()
-
-    if search_string:
-        gene_list = gene_list[gene_list["gene_names"].str.contains(search_string, case=False)]
-
-    return templates.TemplateResponse('display_home.html', context = {'request' : request, 'genes_df' : gene_list})
-
-# For count barplots
-@app.get("/csinc/plots/barplot/{gene_id}/")
-async def bar_plot(request: _fastapi.Request, gene_id: str):
-    counts_dict, gene_id = await _services.fetchCounts(gene_id)
-    
-    await _services.fetchCountsPlot(counts_dict, gene_id)
-
-    return templates.TemplateResponse(gene_id + "_counts_log1p" + ".html", context={'request' : request})
-
-# For count boxplots
-@app.get("/csinc/plots/boxplot/{gene_id}/")
-async def box_plot(request: _fastapi.Request, gene_id: str):
-    counts_dict, gene_id = await _services.fetchCounts(gene_id)
-    
-    await _services.fetchCountsBoxPlot(counts_dict, gene_id)
-
-    return templates.TemplateResponse(gene_id + "_counts_boxplot_log1p" + ".html", context={'request' : request})
-
-# For intra-sample variance
-@app.get("/csinc/plots/intravar/{gene_id}/")
-async def intravar_box_plot(request: _fastapi.Request, gene_id: str):
-    counts_dict, gene_id = await _services.fetchCounts(gene_id)
-    
-    await _services.fetchCountsIntraVariancePlot(gene_id)
-
-    return templates.TemplateResponse(gene_id + "_counts_intravar_box_log1p" + ".html", context={'request' : request})
-
-# For intra-sample variance non-zero filtered
-@app.get("/csinc/plots/intravarnz/{gene_id}/")
-async def intravar_box_plot(request: _fastapi.Request, gene_id: str):
-    counts_dict, gene_id = await _services.fetchCounts(gene_id)
-    
-    await _services.fetchCountsIntraVariancePlot(gene_id, zero_filt=True)
-
-    return templates.TemplateResponse(gene_id + "_counts_intravar_box_log1p" + ".html", context={'request' : request})
-"""
